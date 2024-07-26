fb3cc1a788c15e54a80778cbe7d7df803083f7f4
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-07-26 22:24:08 +0200

    configure login page

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def register():
     return render_template("register.html", title="Register", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     return render_template("login.html", title="Login", form=form)
