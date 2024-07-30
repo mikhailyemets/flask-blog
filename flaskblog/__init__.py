f72c98e44405f2ad7376f33570d729d0e4b8035a
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-07-30 19:35:51 +0200

    split files to diferent folders

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -8,27 +8,29 @@ from flaskblog.filters import safe_truncate
 from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
-# email configuration
-app.config["MAIL_SERVER"] = "smtp.googlemail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASSWORD")
-mail = Mail(app)
+
 
 # add func for safe truncate of words
 app.jinja_env.filters["safe_truncate"] = safe_truncate
 
 # to make account login required
-login_manager.login_view = "login"
+login_manager.login_view = "users.login"
 
 # to make nice message about login in
 login_manager.login_message_category = "info"
 
-from flaskblog import routes
+# Blueprints configuration
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
+
+mail = Mail(app)
--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,12 +1,14 @@
 from flask_mail import Message
 from flask import url_for
-from flaskblog import mail
+from flaskblog import app, mail
 from flaskblog.models import User
-
+import os
+import secrets
+from PIL import Image
 
 def send_reset_email(user: User) -> None:
     token = user.get_reset_token()
-    reset_url = url_for("reset_token", token=token, _external=True)
+    reset_url = url_for("users.reset_token", token=token, _external=True)
     msg = Message(
         "Password reset request",
         sender="noreply@demo.com",
@@ -17,7 +19,7 @@ def send_reset_email(user: User) -> None:
 
     We received a request to reset your password. You can reset your password by clicking the link below:
 
-    {url_for("reset_token", token=token, external=True)}
+    {url_for("users.reset_token", token=token, external=True)}
 
     If you did not request this password reset, please ignore this email. Your password will not be changed.
 
@@ -32,3 +34,22 @@ def send_reset_email(user: User) -> None:
        <p>Thank you,<br>The FlaskBlog Team</p>
        """
     mail.send(msg)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join(
+        app.root_path,
+        "static/profile_pics",
+        picture_filename
+    )
+
+    #resing big pictures to keep it small
+    output_size = (125,125)
+    input_ = Image.open(form_picture)
+    input_.thumbnail(output_size)
+
+    input_.save(picture_path)
+    return picture_filename
